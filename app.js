dcf5fa5f2cf0d8c47933ddbac798606f114969ad
Jarviss77 <gupta.shubham2404@gmail.com>
2024-02-15 17:21:51 +0530

    Prisma Initiated

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import { connDB } from './config/db.config.js';
+import { ConnDB } from './config/db.config.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}! 🚀`);
+    ConnDB();
+});
