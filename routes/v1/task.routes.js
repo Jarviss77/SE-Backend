4bfc362defa3c210d2497ec0be9984c6ed9a8e2f
Jarviss77 <gupta.shubham2404@gmail.com>
2024-04-21 00:07:04 +0530

    minor change

--- routes/v1/task.routes.js
+++ routes/v1/task.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createTask, assignTask, getUnassignedTasks } from '../../controllers/task.controllers.js';
 import { authVerify } from '../../middlewares/auth.middlewares.js';
+import { isAssigner } from '../../middlewares/isAssigner.middlewares.js';
 
 const router = express.Router();
 
-router.route('/create').post(authVerify, createTask);
+router.route('/create').post(authVerify, isAssigner, createTask);
 router.route('/assign/:id').patch(authVerify, assignTask);
 router.route('/getUnassignedTasks/:id').get(authVerify, getUnassignedTasks);
 
