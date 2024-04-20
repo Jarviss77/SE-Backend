ed5ef02175acf5b92cfe7057f8af8d63f4af8e4c
harshsingh554 <72743057+harshsingh554@users.noreply.github.com.>
2024-04-20 22:41:49 +0530

    Updated assignTask route

--- routes/v1/task.routes.js
+++ routes/v1/task.routes.js
@@ -5,7 +5,7 @@ import { authVerify } from '../../middlewares/auth.middlewares.js';
 const router = express.Router();
 
 router.route('/create').post(authVerify, createTask);
-router.route('/assign/:id').post(authVerify, assignTask);
+router.route('/assign/:id').patch(authVerify, assignTask);
 router.route('/getUnassignedTasks/:id').get(authVerify, getUnassignedTasks);
 
 export default router;
