f783236acc3336d0cac36b1aaed3bb8d1a0c5dd8
thehackerash <aayushprajapati0807@gmail.com>
2024-04-25 03:49:22 +0530

    bug resolve

--- routes/v1/v1-main.js
+++ routes/v1/v1-main.js
@@ -3,7 +3,6 @@ import express from 'express';
 import { default as userAuthRouter } from './auth.routes.js'
 import {default as organisationRouter} from './organisation.routes.js'
 import { default as getUserRouter } from './user.routes.js'
-import { default as testRouter } from './testing.routes.js'
 import { default as taskRouter } from './task.routes.js'
 import { default as imageRouter } from './image.routes.js'
 import { default as memberRouter } from './member.routes.js';
@@ -16,6 +15,5 @@ router.use('/user', getUserRouter);
 router.use('/task', taskRouter);
 router.use('/image', imageRouter);
 router.use('/member', memberRouter)
-router.use('/test', testRouter);
 
 export default router;
