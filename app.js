c93bd6acebf3dc06f06a241bac4404b325d5b4c8
Om Buddhadev <128999558+0mBudsMan@users.noreply.github.com>
2024-05-03 23:30:30 +0530

    allowed cors for vercel fronted

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000', 'https://se-frontend-pearl.vercel.app/auth/login'
   }));
 app.use(morgan('dev'));
 
